a6f4cb5b62318af37000a291af9268c0f562d545
Andres Pena <penchjpc@gmail.com>
2022-02-01 21:39:03 -0500

    testing netlify continuos deployment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+         Wohooo, we did it!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
