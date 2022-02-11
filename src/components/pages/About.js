a470c57973696c0cf6498207c8d67aae560ca7ce
Andres Pena <penchjpc@gmail.com>
2022-02-11 18:05:10 -0500

    tweaked font, paddings and brand statement

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -16,12 +16,12 @@ function About() {
                 <div className="col-12 col-sm-6 col-md-8 col-lg-8">
                   <p id="about-me">                        
                        Front-end web developer leveraging a background in transportation engineering to provide unique perspectives on 
-                       how web applications can help cities and companies move more efficiently, safely, and fairly. Problem-solver and
-                       avid learner with Bachelor and Masters degrees in Civil Engineering. Recently earned a Certificate in 
+                       how web applications can help companies and clients grow more efficiently. Integral problem-solver and
+                       passionate learner with Bachelor and Masters degrees in Civil Engineering. Recently earned a Certificate in 
                        Full-stack Development from the University of Toronto, and acquired newly developed skills in JavaScript, 
-                       CSS, React.js, SQL, MongoDB, Express.js and Node.js Strengths in creativity, responsibility, teamwork, and building 
-                       projects from ideation to execution. <em>To learn more about my skills go to the <strong>Resume</strong> section.</em>
-                       </p>    
+                       CSS, React.js, SQL, MongoDB, Express.js and Node.js. Strengths in curiosity, drive to grow, responsibility, teamwork, and management 
+                       of multiple priorities. <em>To learn more about my skills go to the Resume page.</em>
+                  </p>    
               </div>
             </div>
         </div>
