baa4d3f5d6957023fdded634a7077984ff903a3e
Andres Pena <penchjpc@gmail.com>
2022-02-11 20:57:05 -0500

    refactored styles for header and footer and :hover,:focus pseudo-classes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -20,7 +20,7 @@ function About() {
                        passionate learner with Bachelor and Masters degrees in Civil Engineering. Recently earned a Certificate in 
                        Full-stack Development from the University of Toronto, and acquired newly developed skills in JavaScript, 
                        CSS, React.js, SQL, MongoDB, Express.js and Node.js. Strengths in curiosity, drive to grow, responsibility, teamwork, and management 
-                       of multiple priorities. <em>To learn more about my skills go to the Resume page.</em>
+                       of multiple priorities. To learn more about my skills, don't forget to checkout the Resume page by clicking on the menu above.
                   </p>    
               </div>
             </div>
